32c49f270eface281c69fbee8e9235de84a6850a
young <18829026282@163.com>
2018-05-15 00:27:46 +0800

    make a ship

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """存储游戏设置类"""
+
+    def _init_(self):
+        """初始化游戏设置"""
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
